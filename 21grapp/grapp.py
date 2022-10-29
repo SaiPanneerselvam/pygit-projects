2327f5bf9b6eab8d3f23e6fe339853ea799fe6c6
Sai Panneerselvam <89541946+SaiPanneerselvam@users.noreply.github.com>
2022-10-29 19:54:15 -0400

    Create grapp.py
    grapp.py created by griffmonstar

---
+++ 21grapp/grapp.py
@@ -0,0 +1,11 @@
+# THIS IS A LIBRARY, NOT A FILE
+# 2004 GRIFFAPPS MIT LICENCED \
+def compile(var):
+  if isinstance(var, str) = False:
+    print("computer error. Value was not a string to compress. Value must be a string.")
+def save(var1):
+    var1 = saveslot
+    print("saved")
+def load(var2)
+    saveslot = var2
+    print("loaded into var2")
